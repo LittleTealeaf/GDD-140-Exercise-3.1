4181eb645d14b4acb303b3c40ffbbf7bd820f5a5
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-22 15:04:29 -0500

    added comments

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
+/**
+ * Creates a button object that contains the location, dimensions, and a draw method
+ */
 class Button {
   constructor(x,y,w,h) {
     this.x = x;
@@ -8,28 +11,45 @@ class Button {
     this.h = h;
   }
 
+  /**
+   * Calls the rect method and draws the rectangle
+   */
   draw() {
     rect(this.x,this.y,this.w,this.h);
   }
 
+  /**
+   * Returns whether or not the xa nd y values is on the button
+   * @param {*} x 
+   * @param {*} y 
+   * @returns 
+   */
   contains(x,y) {
     return this.x < x && x < this.x + this.w && this.y < y && y < this.y + this.h;
   }
 }
 
+//Current button
 var button;
+//The current display shape
 var state = 0;
+
 
 function setup() {
   createCanvas(500, 500);
+  //Initialize new button
   button = new Button(width/2-width/4,height/2,75,75);
 }
 
 function draw() {
+  //Draw background
   background(220);
+
+  //Draw the button
   fill("#B9116D");
   button.draw();
 
+  //draw the specific shape based on the state
   fill("#D75710")
   var x = width / 2 + width / 4;
   var y = width / 2;
@@ -43,8 +63,10 @@ function draw() {
   }
 }
 
+
 function mouseClicked() {
   if(button.contains(mouseX,mouseY)) {
+    //Loop the state between 0, 1, and 2
     state = (state + 1)% 3;
   }
 }
